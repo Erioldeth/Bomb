eeaa7fdc4c91148415e8e71ddab1ae42eef3e3d2
Erioldeth <crystaleye005@gmail.com>
2022-11-09 08:04:13 +0700

    new commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module com.github.erioldeth.enchantedbomb {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires org.controlsfx.controls;
+	requires com.almasb.fxgl.all;
+	requires annotations;
+	exports com.github.erioldeth.bomb;
+}
